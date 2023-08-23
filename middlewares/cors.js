f1a2a8e6476c6be7a92daab49ae755964955e6a9
Mila <milanaganaga@yandex.ru>
2023-08-24 03:35:49 +0400

    backend

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,8 +1,6 @@
 const allowedCors = [
   'https://pr-movies.nomoreparties.co',
   'http://pr-movies.nomoreparties.co',
-  // 'https://pr-movies-explorer.nomoreparties.co',
-  // 'http://pr-movies-explorer.nomoreparties.co',
   'localhost:3000',
 ];
 
@@ -12,15 +10,44 @@ module.exports = (req, res, next) => {
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   const requestHeaders = req.headers['access-control-request-headers'];
 
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', '*');
-  }
-
   if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
     return res.end();
   }
 
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', '*');
+  }
+
   return next();
 };
+
+// const allowedCors = [
+
+//   'https://pr-movies.nomoreparties.co',
+//   'http://pr-movies.nomoreparties.co',
+//   // 'https://pr-movies-explorer.nomoreparties.co',
+//   // 'http://pr-movies-explorer.nomoreparties.co',
+//   'localhost:3000',
+// ];
+
+// module.exports = (req, res, next) => {
+//   const { origin } = req.headers;
+//   const { method } = req;
+//   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+//   const requestHeaders = req.headers['access-control-request-headers'];
+
+//   if (allowedCors.includes(origin)) {
+//     res.header('Access-Control-Allow-Origin', '*');
+//   }
+
+//   if (method === 'OPTIONS') {
+//     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+//     res.header('Access-Control-Allow-Headers', requestHeaders);
+//     return res.end();
+//   }
+
+//   return next();
+// };
